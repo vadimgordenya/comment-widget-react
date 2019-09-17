cb5e4f0749440b18d0369cbf812acff160fa27e0
Vadim <vadimgordenya@gmail.com>
2019-09-17 10:51:13 +0300

    comment-widget-react

---
+++ precache-manifest.d796eb7fa409e1b460d253707056cb24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7166a7953cfceb537273171f7852c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f1c1422b5961fde533",
+    "url": "/static/css/main.5e97494c.chunk.css"
+  },
+  {
+    "revision": "4bbac782c0048f0eaf58",
+    "url": "/static/js/2.95350df8.chunk.js"
+  },
+  {
+    "revision": "99f1c1422b5961fde533",
+    "url": "/static/js/main.b9b75983.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
